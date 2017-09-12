f37895c0738d1d3c9c7bde3f8ab59863b3c7ee51
Junzhi Chen <chenjunz@IITS-B473-30.utsc-labs.utoronto.ca>
2017-09-12 12:48:36 -0400

    B change 2nd line

--- tut1.py
+++ tut1.py
@@ -1,5 +1,5 @@
 def tut():
-    print("this is tut1,and i change this line.")
+    print("this is tut1,and B change this line.")
     print("at least 5 lines, I am B.")
     print("blablablaha")
 tut()
