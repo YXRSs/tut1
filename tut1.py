0b7e614307ad90f4d70187863b50af09bedad34e
Zongyang Li <lizongya@IITS-B473-29.utsc-labs.utoronto.ca>
2017-09-12 12:42:59 -0400

    A change 2 line

--- tut1.py
+++ tut1.py
@@ -1,5 +1,5 @@
 def tut():
     print("this is tut1,and i change this line.")
-    print("at least 5 lines")
+    print("at least 5 lines. I am A, I change this line.")
     print("blablablaha")
 tut()
