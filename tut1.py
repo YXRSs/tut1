b43a13158dc64aa061ca584d8237d01832aa5e1c
Junzhi Chen <chenjunz@IITS-B473-30.utsc-labs.utoronto.ca>
2017-09-12 12:52:37 -0400

    B change 4th line

--- tut1.py
+++ tut1.py
@@ -2,5 +2,5 @@ def tut():
     print("this is tut1,and i change this line.")
     print("this is tut1,and B change this line againnnnn.")
     print("at least 5 lines, I am B.")
-    print("blablablaha")
+    print("blablablaha, B change")
 tut()
